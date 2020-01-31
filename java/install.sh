4a00ef628d04ac5c60ccf36f837e1817b7e3ae23
kwan <jarupong.wiboonchart@krungsri.com>
2020-01-31 23:42:42 +0700

    add java and tool dev

---
+++ java/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#
+# This installs some of the common dependencies needed (or at least desired)
+
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
+brew cask install intellij-idea-ce
+brew cask install postman
+brew cask install visual-studio-code
+brew install mas
+brew cask install vlc
+mas install 539883307 #line
+exit 0
